2b3059602662647d525862d2a2b999f3cf1a3395
Irvin <irvinhomem@gmail.com>
2019-03-11 16:46:45 +0100

    Testing xml and network graphs

--- XMLParsingTest.py
+++ XMLParsingTest.py
@@ -1,5 +1,8 @@
 import xml.etree.ElementTree
 import os
+import networkx as nx
+import matplotlib.pyplot as plt
+import xmltodict
 
 from lxml import etree, objectify
 from bs4 import BeautifulSoup
@@ -13,6 +16,8 @@ parser = etree.XMLParser(remove_comments=True, recover=True )
 tree = objectify.parse(xml_file_path, parser=parser)
 print('Type: %s ' % type(tree) )
 
+#xml_dict = xmltodict.parse(xml_file_path)
+
 root = tree.getroot()
 print('Root: %s' % tree.getroot())
 
@@ -23,9 +28,12 @@ print('Child Element Rules --> List Length: %i' % len(my_rules))
 rules_children = root.getchildren()
 print('Type expected for Rules Children -->List: %s' % type(rules_children))
 print('Rules Children --> List Length: %i' % len(rules_children))
+print('Rules Children ELEMENT exprected type: %s' % type(rules_children[0]))
+
 print('RULES: %s' % rules_children  )
 
-# Elements carrt attributes as a dict
+
+# Elements carry attributes as a dict
 print(rules_children[0].tag)
 print(rules_children[0].keys())
 print(rules_children[0].get('name'))
@@ -43,10 +51,31 @@ print('Rules list: %s' % rules)
 print('Rules list: %s' % rules_all)
 
 
+# Adding XML elements to a Graph
+my_graph = nx.Graph()
+#my_graph.add_nodes_from(rules_children, data=True)
+my_graph.add_nodes_from(rules_children)
+print('My GRAPH nodes NUMBER: %s' % my_graph.number_of_nodes())
+print('Nodes ALL Type: %s' % type(my_graph.nodes))
+print('NODE ... ALL should be dictionary: %s' % (my_graph.nodes))
+
+print('-----------------')
+print('Nodes ALL should be dictionary: %s' % type(my_graph.node))
+print('Nodes ALL should be dictionary: %s' % type(my_graph.node.data))
+print('NODE ... ALL should be dictionary: %s' % (my_graph.node))
+print('-----------------')
+print('List with DATA: %s' % list(my_graph.nodes.data()))
+
+
+
+
+# Works
+nx.draw_circular(my_graph, with_labels=True)
+#nx.draw_kamada_kawai(my_graph, with_labels=True)
+plt.show()
+
 #my_tree = etree.ElementTree(tree)
-
 #soup = BeautifulSoup(tree, 'lxml')
-
 #lxml
 # for node in tree.iter('*'):
 #     print(node.tag)
